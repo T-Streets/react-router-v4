0b2f2678d3db9239aa41bfe332b7decbfd015c0a
T-Streets <tyler@featherandash.io>
2019-03-15 10:29:58 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SpaceCowboy from './components/SpaceCowboy'
+import PassProps from './components/PassProps'
+import Sidebar from './components/Sidebar'
+import { Route, Link, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+)
+
+const Matched = () => (
+  <div>
+    <h1>Matched Path</h1>
+  </div>
+)
+/**
+ * NoMatch would be your 404 page/component
+ */
+const NoMatch = ({ location}) => (
+  <div>
+    <h1>This isnt the page youre looking for</h1>
+    <h3>There is no path for <code>{location.pathname}</code></h3>
+  </div>
+)
+
 
 class App extends Component {
+  state = {
+    secretMessage: 'foxtrot uniform charlie kilo'
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <div>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/hunters'>SpaceCowboy</Link></li>
+              <li><Link to={{
+                pathname: '/pass-props',
+                state: {
+                secretMessage: this.state.secretMessage
+                },
+              }}>Pass Props To Components</Link></li>
+              <li><Link to='/matched'>Matched</Link></li>
+              <li><Link to='/old-page'>Old page to be redirected</Link></li>
+              <li><Link to='/sidebar'>Sidebar</Link></li>
+            </ul>
+
+            <hr />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Redirect from='/old-page' to='/matched' />
+              <Route path='/hunters' component={SpaceCowboy} />
+              <Route path='/pass-props' component={PassProps} />
+              <Route path='/matched' component={Matched} />
+              <Route path='/sidebar' component={Sidebar} />
+              <Route component={NoMatch} />
+            </Switch>
+            {/* when a route is passed without a path it will always be rendered if the path in url is not exact ie 
+                any path that does not match what we have set, will be forwarded to this route
+            */}
+          </div>
+        </Router>
       </div>
     );
   }
