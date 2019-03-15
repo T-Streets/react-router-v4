98cd1882d8353cdb21d425562f44b7c4d058158f
T-Streets <tyler@featherandash.io>
2019-03-15 11:12:21 -0700

    adds sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,10 +5,65 @@ import {
     Route
 } from 'react-router-dom'
 
+const routes = [
+    {
+        path: '/',
+        exact: true,
+        sidebar: () => <div>home</div>,
+        main: () => <h2>Home</h2>
+    },
+    {
+        path: '/surf',
+        sidebar: () => <div>surfboard</div>,
+        main: () => <h2>Surfing!</h2>
+    },
+    {
+        path: '/snow',
+        sidebar: () => <div>snowboard</div>,
+        main: () => <h2>Snowboarding!</h2>
+    },
+]
+
 export default class Sidebar extends Component{
     render() {
         return(
-            <h1>Sidebar</h1>
+            <Router>
+                <div style={{display: 'flex'}}>
+                    <div style={{
+                        padding: '10px', 
+                        width: '40%',
+                        background: '#f0f0f0'
+                        }}>
+                        <ul style={{listStyleType: 'none', padding: 0}}>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/surf'>Surfing</Link></li>
+                            <li><Link to='/snow'>Snowboarding</Link></li>
+                        </ul>
+                        {
+                            routes.map(route => (
+                                <Route 
+                                    key={route.path}
+                                    path={route.path}
+                                    exact={route.exact}
+                                    component={route.sidebar}
+                                />
+                            ))
+                        }
+                    </div>
+                    <div style={{flex: 1, padding: '10px'}}>
+                        {
+                            routes.map(route => (
+                                <Route
+                                    key={route.path}
+                                    path={route.path}
+                                    exact={route.exact}
+                                    component={route.main}
+                                />
+                            ))
+                        }
+                    </div>
+                </div>
+            </Router>
         )
     }
 }
